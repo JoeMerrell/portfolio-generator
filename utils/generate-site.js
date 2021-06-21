1bbfb0f2be8d27ab0b0924bda538cf8d4bdd636e
Joseph <joe@uhhuhohyeah.com>
2021-06-20 21:04:30 -0700

    refactored the code

---
+++ utils/generate-site.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+// writing files
+const writeFile = fileContent => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    });
+  });
+};
+
+// good examples of JavaScript Promises above and below
+
+// copying file
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'Stylesheet created!'
+      });
+    });
+  });
+};
+
+module.exports = { writeFile, copyFile };
+
+// can also be
+
+// module.exports = {
+//   writeFile: writeFile,
+//   copyFile: copyFile
+// };
